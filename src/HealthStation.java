752b8b116fc1adb795db1928847ba83fb28304f4
ShahromDehoti <shahrom.dehoti1999@gmail.com>
2025-12-20 06:30:45 -0500

    add feed method to HealthStation class

--- src/HealthStation.java
+++ src/HealthStation.java
@@ -3,11 +3,24 @@ public class HealthStation {
         return person.getWeight();
     }
 
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+
     public static void main(String[] args) {
         HealthStation childrensHospital = new HealthStation();
 
         Person ethan = new Person("Ethan", 1, 110, 7);
         Person peter = new Person("Peter", 33, 176, 85);
+
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+
+        System.out.println("");
 
         System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
         System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
