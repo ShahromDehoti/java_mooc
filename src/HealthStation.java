9ccc71cfa9934fd69aebff69e34fe9651af4cfce
ShahromDehoti <shahrom.dehoti1999@gmail.com>
2025-12-20 06:23:42 -0500

    HealthStation class added

---
+++ src/HealthStation.java
@@ -0,0 +1,15 @@
+public class HealthStation {
+    public int weigh(Person person) {
+        return person.getWeight();
+    }
+
+    public static void main(String[] args) {
+        HealthStation childrensHospital = new HealthStation();
+
+        Person ethan = new Person("Ethan", 1, 110, 7);
+        Person peter = new Person("Peter", 33, 176, 85);
+
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+    }
+}
